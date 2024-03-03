5f2b73648ceb63409d6b90cae463a65f95f9a2ee
raghavgupta4475 <raghavkumargupta18@gmail.com>
2024-03-03 13:53:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 let startTime;
 let endTime;
-
+let intervalId;
 function App() {
 
   const [time, setTime] = useState("0:00");
@@ -12,28 +12,34 @@ function App() {
   const start = () => {
     setIsRunning(true);
     startTime = new Date();
-  }
-
+    intervalId = setInterval(updateTime, 1000);
+  };
 
   const stop = () => {
+    setIsRunning(false);
+    clearInterval(intervalId);
+  };
+
+  const updateTime = () => {
+
     endTime = new Date();
     const timeDiff = endTime - startTime;
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    setTime(`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`);
-    setIsRunning(false);
-  }
+    const seconds = Math.floor((timeDiff / 1000) % 60);
+    const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+    setTime(`${minutes}:${seconds.toString().padStart(2, '0')}`);
+
+  };
 
   return (
     <>
       <h3>Stopwatch</h3>
       <p>Time: {time} </p>
-      {isRunning ? (
-        <button onClick={stop}>Stop</button>
-      ) : (
-        <button onClick={start}>Start</button>
-      )}
-      <button onClick={() => setTime("0:00")}>Reset</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={start}>Start</button>
+      <button onClick={() => {
+        setTime("0:00");
+        clearInterval(intervalId);
+      }}>Reset</button>
     </>
   );
 }
